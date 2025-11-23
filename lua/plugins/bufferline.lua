326fd332e7ce65a527c8939e83e3563da8ace5ae
TheShaicho <bereveskos@me.com>
2025-11-23 21:21:24 +0200

    Enhance bufferline setup with icons and highlights
    Updated bufferline configuration to include close icons and modified icon colors.

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,21 +1,48 @@
 return {
-	{ "nvim-tree/nvim-web-devicons", lazy = true },
+    { "nvim-tree/nvim-web-devicons", lazy = true },
 
-	{
-		"akinsho/bufferline.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		version = "*",
-		config = function()
-			require("bufferline").setup({
-				options = {
-					mode = "buffers",
-					path_components = 1,
-					show_buffer_close_icons = false,
-					show_close_icon = false,
-					separator_style = "thin",
-					always_show_bufferline = true,
-				},
-			})
-		end,
-	},
+    {
+        "akinsho/bufferline.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        version = "*",
+        config = function()
+            require("bufferline").setup({
+                options = {
+                    mode = "buffers",
+                    path_components = 1,
+                    separator_style = "thin",
+                    always_show_bufferline = true,
+
+                    show_buffer_close_icons = true, 
+                    show_close_icon = false, 
+
+                   
+                    buffer_close_icon = '●',
+                    modified_icon = '●',
+                },
+                
+                highlights = {
+                    close_button = {
+                        fg = '#ff5555',
+                    },
+                    close_button_selected = {
+                        fg = '#ff5555',
+                    },
+                    close_button_visible = {
+                        fg = '#ff5555',
+                    },
+
+                    modified = {
+                        fg = '#ffb86c',
+                    },
+                    modified_selected = {
+                        fg = '#ffb86c',
+                    },
+                    modified_visible = {
+                        fg = '#ffb86c',
+                    },
+                }
+            })
+        end,
+    },
 }
